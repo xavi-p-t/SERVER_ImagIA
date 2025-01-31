53410e0e535315a7efb70b8be4bdcd33a5298fad
xuanor <joancomic.jm@gmail.com>
2025-01-31 18:55:23 +0100

    Update imageController.js

--- api/src/controllers/imageController.js
+++ api/src/controllers/imageController.js
@@ -60,7 +60,7 @@ const uploadImage = async (req, res) => {
 // Función para enviar la solicitud a la API del modelo
 const sendToMarIA = async (jsonBody) => {
     try {
-        const response = await fetch('http://localhost:11434/api/generate', {
+        const response = await fetch('http://192.168.1.14:11434/api/generate', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(jsonBody),
